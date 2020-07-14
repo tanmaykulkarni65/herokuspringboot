cee147089f17a1ab73ba090fab6ce9afed3bae4c
Tanmay Kulkarni <tanmay.kulkarni@lexiconnetworks.com>
2020-07-14 19:21:42 +0530

    Initial Commit

---
+++ src/main/java/com/lexicon/DemoHerokuApplication.java
@@ -0,0 +1,15 @@
+package com.lexicon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHerokuApplication.class, args);
+	}
+	
+	
+
+}
